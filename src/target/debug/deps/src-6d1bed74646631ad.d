e825a65bb990830011295e25ea5754044fd5d7ec
fonsow <afonso.resendep@gmail.com>
2024-01-18 20:05:40 +0000

    test

--- src/target/debug/deps/src-6d1bed74646631ad.d
+++
@@ -1,5 +0,0 @@
-/home/fonsow/distributedLedger/src/target/debug/deps/src-6d1bed74646631ad: main.rs
-
-/home/fonsow/distributedLedger/src/target/debug/deps/src-6d1bed74646631ad.d: main.rs
-
-main.rs:
--- src/target/debug/src.d
+++
@@ -1,1 +0,0 @@
-/home/fonsow/distributedLedger/src/target/debug/src: /home/fonsow/distributedLedger/src/main.rs
